6a40e40d6525fce16dabad17fec207f89fcef336
Viktor Valastin <viktorko99@gmail.com>
2020-01-15 17:56:45 +0100

    Added Fira Sans

--- entry/src/App.vue
+++ entry/src/App.vue
@@ -16,7 +16,7 @@ export default class App extends Vue {}
   background-color: #252429;
 } */
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Fira Sans', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
